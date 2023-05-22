80f68fcf52ce7343afaee71d83b4c6e57a11c348
Omkar <omkarpatil2254@gmail.com>
2023-05-22 18:11:00 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -4,29 +4,61 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import Alert from './components/Alert';
+import About from './components/About';
+// import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 
 function App() {
 
   const[mode, setMode] = useState('light'); //Whether dark mode is enabled or not
+  // const[alert, setAlert] = useState(null);
+
+  // const showAlert = (message, type)=>{
+  //   setAlert({
+  //     msg: message,
+  //     type: type
+  //   })
+  // }
+
   const toggleMode = () => {
     if(mode==='light'){
       setMode('dark')
       document.body.style.backgroundColor = "#052c65"
+      // showAlert("Dark mode has been enabled", "sucess");
     }else{
       setMode('light')
       document.body.style.backgroundColor = "white"
+      // showAlert("Light mode has been enabled", "sucess");
     }
     
   }
   return (
     <>
+    <Router>
     <Navbar title="TextUtils" about="About" mode={mode} toggleMode={toggleMode}/>
-    <Alert alert="This is alert"/>
+    {/* <Alert alert={alert}/> */}
     <div className="container my-3">
-    <TextForm heading="Enter the text to analyze" mode={mode}/>
+    <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+         
+          <Route path="/">
+          <TextForm heading="Enter the text to analyze" mode={mode}/>
+          </Route>
+    </Switch>
+    
+    
+    
     </div>
+    </Router>
     {/* <About/> */}
     </>
     
